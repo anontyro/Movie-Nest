d8c16064209c4032fdf18fd9aaa56eafaf247181
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-05-08 10:39:49 +0800

    added in vue to nest serving still can't serve static img for some reason and still stuck in dist dir next to add more endpoints then start working on the frontend or CI CD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { MovieDbController } from './movie-db/movie-db.controller';
 import { MovieDbService } from './movie-db/movie-db.service';
 import { HttpFetchService } from './http-fetch/http-fetch.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
+      exclude: ['/api*', '/public*'],
+    }),
+  ],
   controllers: [AppController, MovieDbController],
   providers: [AppService, MovieDbService, HttpFetchService],
 })
--- src/movie-db/movie-db.controller.ts
+++ src/movie-db/movie-db.controller.ts
@@ -4,7 +4,6 @@ import { MovieDbService } from './movie-db.service';
 @Controller('movies')
 export class MovieDbController {
   constructor(private readonly movieDbService: MovieDbService) {}
-  // get now playing
   @Get('now-playing')
   getNowPlaying(@Query() page?): Promise<any> {
     return this.movieDbService.getNowPlaying(page);
