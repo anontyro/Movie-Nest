f44898cfa0b09ea48b584fa29e37368f0cc318e2
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-05-10 12:19:56 +0800

    moved carousel into its own component need to add detail page

---
+++ movie-lib-frontend/src/components/carousel/FullWidthCarousel.vue
@@ -0,0 +1,70 @@
+<template>
+    <Carousel :autoplay="4000" :wrap-around="true">
+    <slide v-for="movie in movies" :key="movie.id">
+      <div class="slide-item-container">
+        <div class="slide-item-img" :style="{ backgroundImage: `url(${createBgImg(movie)})`}">
+          <div class="slide-title">
+            {{ movie.title }} ({{movie.vote_average}})
+          </div> 
+        </div>
+      </div>
+    </slide>
+    <template #addons>
+      <pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script lang="ts">
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { Options, Vue, prop } from 'vue-class-component';
+import { MovieItem } from '../../@types';
+
+class Props {
+  movies = prop<MovieItem[]>({default: []})
+}
+
+@Options({
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+})
+export default class FullWidthCarousel extends Vue.with(Props) {
+  mounted(){
+    this.movies;
+  }
+  
+  createBgImg = (movie: MovieItem): string => {
+    const baseUrl = `https://image.tmdb.org/t/p/original${movie.backdrop_path ?? movie.poster_path}`;
+    return baseUrl;
+  }
+
+}
+
+</script>
+<style lang="scss" scoped>
+  .slide-item-container{
+    width: 100%;
+    height: 30rem;
+    position: relative;
+    .slide-item-img{
+      height: 100%;
+      width: 100%;
+      background-size: cover;
+      .slide-title{
+        position: absolute;
+        bottom: 0;
+        color: white;
+        left: 0;
+        right: 0;
+        font-size: 2rem;
+        padding: 1rem;
+        font-family: sans-serif;
+        background-color: rgba(0, 0, 0, 0.57);
+      }
+    }
+  }
+</style>
--- movie-lib-frontend/src/views/Home.vue
+++ movie-lib-frontend/src/views/Home.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <button @click.prevent="fetchNowPlayingMovies()" >Get Now Playing</button>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <FullWidthCarousel v-bind:movies="nowPlaying" />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue, setup } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component';
 import { useStore } from '../store';
 import * as MovieActionTypes from '../store/movies/action-types';
+import FullWidthCarousel from '../components/carousel/FullWidthCarousel.vue';
 
+import 'vue3-carousel/dist/carousel.css';
+import { MovieItem } from '../@types';
 
 @Options({
   components: {
-    HelloWorld,
+    FullWidthCarousel,
   },
 })
+
 export default class Home extends Vue {
-  // fetchNowPlayingMovies = setup(() => fetchSetup());
   store = useStore();
+  
+  mounted(){
+    this.fetchNowPlayingMovies();
+  }
 
   fetchNowPlayingMovies = async() => {
       try {
@@ -29,5 +33,34 @@ export default class Home extends Vue {
         console.error('fetch now playing', err);
       }
   };
+
+  get nowPlaying(): MovieItem[] {
+    return this.store.getters.getCurrentlyPlayingMovies ?? [];
+  }
+
+  
 }
 </script>
+<style lang="scss" scoped>
+  .slide-item-container{
+    width: 100%;
+    height: 30rem;
+    position: relative;
+    .slide-item-img{
+      height: 100%;
+      width: 100%;
+      background-size: cover;
+      .slide-title{
+        position: absolute;
+        bottom: 0;
+        color: white;
+        left: 0;
+        right: 0;
+        font-size: 2rem;
+        padding: 1rem;
+        font-family: sans-serif;
+        background-color: rgba(0, 0, 0, 0.57);
+      }
+    }
+  }
+</style>
