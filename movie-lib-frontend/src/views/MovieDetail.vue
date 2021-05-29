09255cc367118acc178a803453988790717e8116
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-05-29 10:14:05 +0800

    added in more movie details

--- movie-lib-frontend/src/views/MovieDetail.vue
+++ movie-lib-frontend/src/views/MovieDetail.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    <span>
-      Movie Id :{{$route.params.id}}
-    </span>
-    <span>
-      Movie Name: {{movieDetails.title}}
-    </span>
-    <span>
-      IMDB ID: {{movieDetails.imdbId}}
-    </span>
+  <div class="movie-detail-container">
+    <div class="movie-header">
+      <div class="movie-banner" :style="{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movieDetails.backdropPath})`}">
+
+        <div class="movie-item" :style="{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movieDetails.posterPath})` }">
+
+        </div>
+      </div>
+      <div class="movie-name">
+        <div class="movie-title">
+          {{movieDetails.title}}
+        </div>
+        </div>
+    </div>
+    <div class="movie-body">
+      <div class="movie-details">{{movieDetails.details}}</div>
+    </div>
   </div>    
 </template>
 
@@ -46,3 +53,41 @@
   }
   </script>
   
+  <style lang="scss" scoped>
+    .movie-header{
+      display: flex;
+      flex-direction: column;
+      height: 400px;
+      .movie-banner{
+        position: relative;
+        width: 100%;
+        height: 300px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        .movie-item{
+          width: 100px;
+          height: 150px;
+          background-size: contain;
+          background-repeat: no-repeat;
+          position: absolute;
+          bottom: -40px;
+          left: 20px;
+          border-radius: 10px;
+          box-shadow: 1px 1px 11px 5px #b60302;
+        }
+      }
+      .movie-name{
+        display: flex;
+        height: 50px;
+        background-color: #b50101;
+        .movie-title{
+          font-family: sans-serif;
+          color: white;
+          font-weight: 700;
+          font-size: 1.5rem;
+          margin: auto;
+        }
+      }
+
+    }
+  </style>
